2181e8a744cd96c11437e8ceda9fa05f64074aef
Freddie Peña <freddy@lingokids.com>
2019-09-09 16:46:59 +0200

    Create plugin

---
+++ plugin.rb
@@ -0,0 +1,13 @@
+# name: Discourse Topic Raw Excerpt
+# about: Add excerpt_raw to Topics list serializer
+# version: 0.1
+# authors: Lingokids
+# url: https://github.com/lingokids
+
+enabled_site_setting :discourse_topic_raw_excerpt_enabled
+
+PLUGIN_NAME ||= "DiscourseTopicRawExcerpt".freeze
+
+after_initialize do
+  add_to_serializer(:listable_topic, :excerpt_raw) { object.excerpt }
+end
